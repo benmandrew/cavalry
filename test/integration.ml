6ea7e6d662558f9b267903ab67ab6ddd4589bd47
benmandrew <benmandrew@gmail.com>
2022-09-29 12:25:11 +0200

    corrected file dependency for integration testing

--- test/integration.ml
+++ test/integration.ml
@@ -2,5 +2,5 @@ open Core
 open Cavalry.Main
 
 let%test_unit "Main.exec" =
-  let result = exec "/home/ben/projects/cavalry/test/test.cvl" in
+  let result = exec "test.cvl" in
   [%test_result: int] result ~expect:182
